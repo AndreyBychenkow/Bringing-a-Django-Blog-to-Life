f8d1bb349b30aff76ff441fca4f60e9da7917ab2
AndreyBychenkow <decebell032@yandex.ru>
2024-11-22 18:35:01 +0300

    add post cards

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,8 @@
 import folium
-
 from django.db.models import Count
 from django.shortcuts import get_object_or_404
 from django.shortcuts import render
+from django.utils.dateformat import format
 
 from blog.models import Comment
 from blog.models import Post
@@ -11,13 +11,10 @@ from sensive_blog.settings import COMPANY_COORDINATES
 
 def serialize_post(post):
     return {
-        "title": post.title,
-        "text": post.text,
-        "author": post.author.username,
-        "comments_amount": Comment.objects.filter(post=post).count(),
-        "image_url": post.image.url if post.image else None,
-        "published_at": post.published_at,
-        "slug": post.slug,
+        'title': post.title,
+        'image_url': post.image.url if post.image else '',
+        'published_date': format(post.published_at, 'F j, Y, P'),
+        'url': post.get_absolute_url(),
     }
 
 
